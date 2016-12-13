a30bf639ed6ccd325b7e9cc217e420af1f689e3f
Leo Wang <bluebreeze0812@outlook.com>
2016-12-13 12:14:36 +0800

    add Baidu Map API

--- views/header.php
+++ views/header.php
@@ -10,3 +10,19 @@
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <meta name="Keywords" content="brandpronounce, brand, speak brand, pronouncebrand">
 <meta name="Description" content="Teach how to pronounce luxury brands correctly.">
+
+<!--Asyncronized load Baidu Map JS API-->
+<script type="text/javascript">
+function initialize() {
+  var mp = new BMap.Map('map');
+  mp.centerAndZoom(new BMap.Point(121.491, 31.233), 11);
+}
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.src = "http://api.map.baidu.com/api?v=2.0&ak=sOAH5cfe70liTMMfZdl6haN6iAcOwvHC&callback=initialize";
+  document.body.appendChild(script);
+}
+   
+window.onload = loadScript; 
+</script>
